0c4e5036a2f06e872228977a781eccc187f81e3d
Suyog Soti <ssoti2201@bvsd.org>
2015-07-16 19:14:09 -0600

    Initial Commit

---
+++ header.php
@@ -0,0 +1,4 @@
+<a href="/hdevtech"><nav>Home</nav></a>
+<a href="/hdevtech"><nav>Products</nav></a>
+<a href="/hdevtech"><nav>About Us</nav></a>
+<a href="/hdevtech"><nav>Contact Us</nav></a>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <link href="styles/style.css" rel="stylesheet" type="text/css">
+        <script src="scripts/jquery-1.11.1.js" type="text/javascript"></script>
+        <script src="scripts/script.js" type="text/javascript"></script>
+        <title>HDevTech</title>
+    </head>
+    <body>
+        <div class="wrapper">
+            <header>
+                <?php include("header.php");?>
+            </header>
+            <div class="side_bar">
+                <h1>Side Bar</h1>
+            </div>
+            <footer>
+                <?php include("footer.php") ?>
+            </footer>
+        </div>
+    </body>
+</html>
