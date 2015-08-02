faaedf67ee72b743ef443f5b34a48c0bc028eb8a
ssoti2201 <ssoti2201@bvsd.org>
2015-08-01 21:03:27 -0600

    Update functions_post.php

--- functions_post.php
+++ functions_post.php
@@ -5,6 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+include('connections.php');
     function post_get($which, $collum){
         $result = mysql_query("SELECT * FROM posts");
         $count = false;
