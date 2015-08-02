b4d4e0aa6f3c5d59c7d414d33ae515a5385cebf9
Suyog Soti <ssoti2201@bvsd.org>
2015-08-01 23:41:44 -0600

    last minute touches

--- footer.php
+++ footer.php
@@ -1,13 +1,9 @@
             <footer>
                 <?php //gotta select the tables from mysql here
-                $info = post_get('title', "Second Post");
-                echo $info[1];
-                $var = "variable change";
-                post_change($var, 2, "body");
-                $info = post_get('id',2);
-                echo $info[2];
+                include("functions_posts.php");
                 ?>
             </footer>
         </div>
     </body>
+    <?php mysql_close($conn)?>
 </html>
--- functions_post.php
+++ functions_post.php
@@ -16,7 +16,7 @@
                 return $array;
             }
         }
-        if($count==false){echo 'The id '.$collum.' does not exist in the table posts';};
+        if($count==false){return 'The id '.$collum.' does not exist in the table posts';};
     }
     function post_change($change, $id_to_change, $collum){
         $sql = "UPDATE posts SET ".$collum."='".$change."' WHERE id=".$id_to_change;
@@ -35,4 +35,30 @@
             }
         }
         if($count==false){echo 'The id '.$collum.' does not exist in the table posts';};
+    }
+    function post_add($add){
+        $sql = "INSERT INTO posts (id) Values (".$add.")";
+        $mysql_query = mysql_query($sql);
+        $result = mysql_query("SELECT * FROM posts");
+        while($row = mysql_fetch_assoc($result)){
+            $var = $row['id'];
+            if($var == $add){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+    function post_delete($id){
+        $sql = "DELETE FROM posts WHERE id=".$id;
+        $query = mysql_query($sql);
+        $result = mysql_query("SELECT * FROM posts");
+        while($row = mysql_fetch_assoc($result)){
+            $var = $row['id'];
+            if($var == $add){
+                return false;
+            }else{
+                return true;
+            }
+        }
     }
